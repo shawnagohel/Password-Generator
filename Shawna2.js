d9829afeb22f9594a7193f5eabbd72d71c09bf8a
Shawna Gohel <shawnagohel@Shawnas-MacBook-Air.local>
2020-08-07 11:49:49 -0700

    update button

--- Shawna2.js
+++ Shawna2.js
@@ -1,12 +1,12 @@
 // DOM Elements
-const resultEl = document.getElementById('result');
+const resultEl = document.getElementById('password');
 const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
-// const clipboardEl = document.getElementById('clipboard');
+
 
 const randomFunc = {
     lower: getRandomLower,
